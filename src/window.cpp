db714f3b357da332b7b69994c07be7bb4e9c51d0
Coding-Ocean <eijo524@gmail.com>
2020-10-18 13:40:11 +0900

    ver1.0

--- src/window.cpp
+++ src/window.cpp
@@ -113,12 +113,3 @@ unsigned getTime(){
     return timeGetTime();
 }
 
-bool msgProc() {
-    present();
-    MSG msg;
-    if( PeekMessage( &msg, 0, 0, 0, PM_REMOVE ) ) {
-        TranslateMessage( &msg );
-        DispatchMessage( &msg );
-    }
-    return ( msg.message != WM_QUIT );
-}
